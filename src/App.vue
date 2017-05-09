47d6d09cc08bac1d84e980c3806a389f4b6ef4ac
lzfei <491714153@qq.com>
2017-05-09 13:10:21 +0800

    添加组件

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,10 @@
 <template>
   <div id="app">
     <div class="layout">
-      <div class="layout-ceiling">
-        <div class="layout-ceiling-main">
-          <a href="#">注册登录</a> |
-          <a href="#">帮助中心</a> |
-          <a href="#">安全中心</a> |
-          <a href="#">服务大厅</a>
-        </div>
-      </div>
-      <div class="layout-header">
-        <Row>
-          <Col span="6"><div class="layout-logo">Claws Pet Vet</div></Col>
-          <Col span="6"><Input class="cpv-search" placeholder="请输入..."></Input></Col>
-          <Col span="2"><Button class="cpv-search-btn" type="primary" icon="ios-search">搜索</Button></Col>
-          <Col span="10"><div class="cpv-call">联系电话：400-675-5462</div></Col>
-        </Row>
-
-      </div>
+      <app-top></app-top>
+      <app-nav></app-nav>
       <router-view></router-view>
+      <app-back-top></app-back-top>
       <div class="layout-copy">
         2011-2016 &copy; Claws Pet Vet
       </div>
@@ -27,69 +13,34 @@
 </template>
 
 <script>
+import AppTop from '@/components/Top'
+import AppNav from '@/components/Nav'
+import AppBackTop from '@/components/BackTop'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    'app-top': AppTop,
+    'app-nav': AppNav,
+    'app-back-top': AppBackTop
+  },
+  data () {
+    return {
+      theme1: 'light'
+    }
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Helvetica Neue", "Luxi Sans", "DejaVu Sans", Tahoma, "Hiragino Sans GB", STHeiti, "Microsoft YaHei";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-.layout{
-  background: #f5f7f9;
-  position: relative;
-  overflow: hidden;
+.ivu-menu li a{
+  color: #657180;
 }
-.layout-logo{
-  width: 190px;
-  height: 30px;
-  float: left;
-  position: relative;
-  top: 15px;
-  left: 60px;
-  font-size: 22px;
-}
-.layout-header{
-  height: 60px;
-  background: #fff;
-  box-shadow: 0 1px 1px rgba(0,0,0,.1);
-}
-.layout-copy{
-  text-align: center;
-  padding: 10px 0 20px;
-  color: #9ea7b4;
-}
-.layout-ceiling{
-  background: #464c5b;
-  padding: 10px 0;
-  overflow: hidden;
-}
-.layout-ceiling-main{
-  float: right;
-  margin-right: 15px;
-}
-.layout-ceiling-main a{
-  color: #9ba7b5;
-}
-.cpv-search{
-  top:15px;
-}
-.cpv-search-btn{
-  top:15px;
-  position: relative;
-  float: left;
-  margin-left: 5px;
-}
-.cpv-call{
-  top:15px;
-  height: 30px;
-  vertical-align: middle;
-  position: relative;
-  font-size: 18px;
-}
+
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,11 +1,113 @@
 <style scoped>
-
+  .layout-1200{
+    width: 1200px;
+    margin: 0 auto;
+  }
+  .f1{
+    margin-top: 50px;
+    height: 350px;
+  }
+  .f1-left{
+    width: 600px;
+    height: 350px;
+    float: left;
+    padding-top: 38px;
+    padding-left: 100px;
+    text-align: left;
+  }
+  .title{
+    font-size: 35px;
+    padding-bottom: 15px;
+  }
+  .sub-title{
+    font-size: 25px;
+    list-style: disc;
+    padding-top: 15px;
+    color: #39f;
+  }
+  .des{
+    font-size: 18px;
+    color: #999;
+    line-height: 28px;
+  }
+  .f2{
+    margin-top: 50px;
+    height: 350px;
+  }
+  .f2-left{
+    float: left;
+    padding-left: 150px;
+    width: 600px;
+  }
+  .f2-right{
+    float: right;
+    text-align: left;
+    padding-right: 150px;
+    padding-top: 50px;
+  }
 </style>
 <template>
-
+  <div>
+    <Carousel autoplay v-model="value2"
+              :autoplay-speed="setting.autoPlaySpeed"
+              :height="setting.height">
+      <Carousel-item>
+        <div class="demo-carousel">
+          <img src="../assets/image/carousel-1.jpg"/>
+        </div>
+      </Carousel-item>
+      <Carousel-item>
+        <div class="demo-carousel">
+          <img src="../assets/image/carousel-2.jpg"/>
+        </div>
+      </Carousel-item>
+      <Carousel-item>
+        <div class="demo-carousel">
+          <img src="../assets/image/carousel-3.jpg"/>
+        </div>
+      </Carousel-item>
+    </Carousel>
+    <div class="layout-1200 f1">
+      <ul class="f1-left">
+        <li class="title">专业爱宠医疗美容</li>
+        <li class="sub-title">宠物医疗</li>
+        <li class="des">专业医师，为爱宠去除病痛</li>
+        <li class="sub-title">宠物美容</li>
+        <li class="des">职业美容医师，让您的小可爱光彩照人</li>
+      </ul>
+      <img class="f1-right" src="../assets/image/hello/hello-1.jpg"/>
+    </div>
+    <div class="layout-1200 f2">
+      <img class="f2-left" src="../assets/image/hello/hello-2.jpg"/>
+      <ul class="f2-right">
+        <li class="title">宠物日常用品大全</li>
+        <li class="sub-title">宠物饲料、玩具、衣物</li>
+        <li class="des">物美价廉，品质有保障</li>
+      </ul>
+    </div>
+    <div class="layout-1200 f1">
+      <ul class="f1-left">
+        <li class="title">健康档案及饲养建议</li>
+        <li class="sub-title">健康状况统计</li>
+        <li class="des">建立爱宠健康档案，关注他们的健康</li>
+        <li class="sub-title">合理化建议</li>
+        <li class="des">专业饲养建议，安全省心</li>
+      </ul>
+      <img class="f1-right" src="../assets/image/hello/hello-3.jpg"/>
+    </div>
+  </div>
 </template>
 <script>
   export default {
-
+    data () {
+      return {
+        value2: 0,
+        setting: {
+          autoPlaySpeed: 3000,
+          height: 550
+        }
+      }
+    }
   }
 </script>
+
